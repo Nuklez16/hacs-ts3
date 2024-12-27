bdafd99435fca64bf6c1830c84bec8cd94ce2c0d
Nuklez16 <riot071@gmail.com>
2024-12-27 12:40:00 +1100

    Update __init__.py

--- custom_components/ts3_sensor/__init__.py
+++ custom_components/ts3_sensor/__init__.py
@@ -1,1 +1,12 @@
 DOMAIN = "ts3_sensor"
+
+async def async_setup_entry(hass, config_entry):
+    """Set up the integration from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
+    return True
+
+async def async_unload_entry(hass, config_entry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(config_entry.entry_id)
+    return True
